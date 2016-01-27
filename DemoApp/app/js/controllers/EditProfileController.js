4aca8946da30fdea68095cb22bb4e3936adbe86f
Bruno Zatta <bzatta@gmail.com>
2016-01-27 10:32:27 -0500

    initial commit, files included are part of angular fundamentals guide

---
+++ DemoApp/app/js/controllers/EditProfileController.js
@@ -0,0 +1,5 @@
+'use strict';
+
+eventsApp.controller('EditProfileController', function($scope) {
+  $scope.user = {};
+});
---
+++ DemoApp/app/js/controllers/EventController.js
@@ -0,0 +1,16 @@
+'use strict';
+
+eventsApp.controller('EventController', function($scope, eventData) {
+
+  $scope.sortOrder = 'name';
+  $scope.event = eventData.event;
+
+  $scope.upVoteSession = function(session) {
+    session.upVoteCount++;
+  };
+
+  $scope.downVoteSession = function(session) {
+    session.upVoteCount--;
+  };
+
+});
